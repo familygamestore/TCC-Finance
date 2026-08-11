33a5c58c37b7f9ce96431e2ad2a42f49ee6ee9bb
familygamestore <iyanhardiyansyah1@gmail.com>
2026-08-11 22:33:55 +0530

    Delete frontend directory

--- frontend/components/events/components/ErrorMessage.tsx
+++
@@ -1,8 +0,0 @@
-interface ErrorMessageProps {
-  message: string;
-}
-
-export default function ErrorMessage({ message }: ErrorMessageProps) {
-  if (!message) return null;
-  return <p className="error">{message}</p>;
-}
