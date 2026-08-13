00f99f39e1d2057984047c8db5d84805e0288771
familygamestore <iyanhardiyansyah1@gmail.com>
2026-08-13 21:14:26 +0530

    Delete frontend directory

--- frontend/app/events/page.tsx
+++
@@ -1,5 +0,0 @@
-import EventsPage from '@/components/events/EventsPage';
-
-export default function Page() {
-  return <EventsPage />;
-}
--- frontend/components/events/components/PageHeader.tsx
+++
@@ -1,8 +0,0 @@
-export default function PageHeader() {
-  return (
-    <>
-      <h1>Event</h1>
-      <p className="subtitle">Kelola event dan lihat profit/loss per event.</p>
-    </>
-  );
-}
